263c3f9fb9ef7e495e985e06cc9470453bab120d
Ibe Arua <ibe@emplug.com>
2021-01-19 08:03:12 +0100

    added heroku procfile and updated the npm script with webpack-dev-server installed

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const request = require("request");
 const url = require("url");
 
+//setup server instance
 const app = express();
 
+
+//api endpoint
 app.get('/api/rates', (req, res)=>{
     const queryObject = url.parse(req.url,true).query;
     request({
         uri: "https://api.exchangeratesapi.io/latest",
         qs:queryObject
-        
+
     }, function(error, response, body){
         const json = JSON.parse(body);
         if(error){
